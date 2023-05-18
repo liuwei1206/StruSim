7bfec2eb8b4c9fc2c8f70d295ef27a459577fb4f
liuwei1206 <yanqing_0301>
2023-05-18 12:16:46 +0200

    preprocess

--- preprocessing.py
+++ preprocessing.py
@@ -4,17 +4,146 @@ import copy
 import os
 import json
 import random
+import csv
 
 random.seed(106524)
 
-def k_fold_for_gcdc(data_dir, dataset):
+def gcdc_csv_reader(dataset, mode):
+    """
+    convert raw csv file into json
+
+    Args:
+        dataset: Clinton or Enron or Yahoo or Yelp
+        mode: train or test
+    """
+    file_name = os.path.join("data/dataset/raw/gcdc", "{}_{}.csv".format(dataset, mode))
+    out_dir = "data/dataset/gcdc_{}".format(dataset.lower())
+    os.makedirs(out_dir, exist_ok=True)
+    out_file = os.path.join(out_dir, "{}.json".format(mode))
+    csv_reader = csv.reader(open(file_name))
+    cur_line_id = 0
+    all_texts = []
+    for line in csv_reader:
+        cur_line_id += 1
+        if cur_line_id == 1:
+            text_idx = line.index("text")
+            label_idx = line.index("labelA")
+            continue
+        sample = {}
+        sample["id"] = cur_line_id - 2
+        sample["score"] = line[label_idx]
+        sample["text"] = line[text_idx]
+
+        all_texts.append(json.dumps(sample, ensure_ascii=False))
+
+    with open(out_file, "w", encoding="utf-8") as f:
+        for text in all_texts:
+            f.write("%s\n"%(text))
+
+def toefl_csv_reader():
+    label_map = {"0": "1", "1": "2", "2": "3"}
+    file_list = os.listdir("data/dataset/raw/toefl")
+    prompt_fold_dict = {}
+    for file_name in file_list:
+        items = file_name.split(".")[0].split("_")
+        mode = items[0]
+        fold_id = int(items[2]) + 1
+        csv_reader = csv.reader(open(os.path.join("data/dataset/raw/toefl", file_name)))
+        cur_line = 0
+        for line in csv_reader:
+            cur_line += 1
+            if cur_line == 1:
+                essay_idx = line.index("essay_id")
+                prompt_idx = line.index("prompt")
+                label_idx = line.index("essay_score")
+                text_idx = line.index("essay")
+                continue
+            prompt = int(line[prompt_idx])
+            sample = {}
+            sample["idx"] = line[essay_idx]
+            sample["score"] = label_map[line[label_idx]]
+            sample["text"] = line[text_idx]
+            sample_text = json.dumps(sample, ensure_ascii=False)
+            token = "{}+{}+{}".format(prompt, fold_id, mode)
+            if token in prompt_fold_dict:
+                prompt_fold_dict[token].append(sample_text)
+            else:
+                prompt_fold_dict[token] = [sample_text]
+
+    for prompt in range(1, 9):
+        data_dir = "data/dataset/toefl_p{}".format(prompt)
+        os.makedirs(data_dir, exist_ok=True)
+        for fold_id in range(1, 6):
+            fold_dir = os.path.join(data_dir, str(fold_id))
+            os.makedirs(fold_dir, exist_ok=True)
+            train_file = os.path.join(fold_dir, "train.json")
+            token = "{}+{}+{}".format(prompt, fold_id, "train")
+            print("%s: %d" % (token, len(prompt_fold_dict[token])))
+            with open(train_file, "w", encoding="utf-8") as f:
+                for text in prompt_fold_dict[token]:
+                    f.write("%s\n"%(text))
+            dev_file = os.path.join(fold_dir, "dev.json")
+            token = "{}+{}+{}".format(prompt, fold_id, "valid")
+            print("%s: %d" % (token, len(prompt_fold_dict[token])))
+            with open(dev_file, "w", encoding="utf-8") as f:
+                for text in prompt_fold_dict[token]:
+                    f.write("%s\n" % (text))
+            test_file = os.path.join(fold_dir, "test.json")
+            token = "{}+{}+{}".format(prompt, fold_id, "test")
+            print("%s: %d" % (token, len(prompt_fold_dict[token])))
+            with open(test_file, "w", encoding="utf-8") as f:
+                for text in prompt_fold_dict[token]:
+                    f.write("%s\n" % (text))
+        
+
+def toefl_csv_reader_v1(file_name, out_dir):
+
+    csv_reader = csv.reader(open(file_name))
+    fold_id = int(file_name.split("/")[-1].split("_")[-1].split(".")[0])
+    all_texts = []
+    for idx in range(1, 9):
+        all_texts.append([])
+    score_map = {"0": "low", "1": "medium", "2": "high"}
+    cur_line_id = 0
+    for line in csv_reader:
+        cur_line_id += 1
+        if cur_line_id == 1:
+            continue
+
+        essay_id = line[0]
+        prompt = line[1]
+        text = line[4]
+        score = line[3]
+
+        all_texts[int(prompt)-1].append((essay_id, text, score))
+    for idx in range(1, 9):
+        cur_dir_name = os.path.join(out_dir, "toefl_p{}".format(idx))
+        os.makedirs(cur_dir_name, exist_ok=True)
+        cur_fold_dir = os.path.join(cur_dir_name, str(fold_id+1))
+        os.makedirs(cur_fold_dir, exist_ok=True)
+
+        out_name = file_name.split("/")[-1].split("_")[0]
+        out_name = "{}.json".format(out_name)
+        out_name = os.path.join(cur_fold_dir, out_name)
+
+        with open(out_name, "w", encoding="utf-8") as f:
+            for idy, item in enumerate(all_texts[idx-1]):
+                sample = {}
+                sample["id"] = idy
+                sample["essay_id"] = item[0]
+                sample["text"] = item[1]
+                sample["score"] = score_map[item[2]]
+
+                f.write("%s\n"%(json.dumps(sample, ensure_ascii=False)))
+
+def k_fold_for_gcdc(dataset):
     """
     k fold split data for dataset
     Args:
         data_dir: data/dataset
         dataset: clinton, enron, yelp, yahoo
     """
-    ori_data_dir = os.path.join(data_dir, dataset)
+    ori_data_dir = os.path.join("data/dataset", "gcdc_{}".format(dataset.lower()))
     train_file = os.path.join(ori_data_dir, "train.json")
     lows = []
     mediums = []
@@ -66,26 +195,20 @@ def k_fold_for_gcdc(data_dir, dataset):
             else:
                 train_samples.extend(ten_group_samples[idy])
 
-        group_data_dir = os.path.join("k_fold_gcdc", dataset)
-        group_data_dir = os.path.join(group_data_dir, str(idx))
+        group_data_dir = os.path.join(ori_data_dir, str(idx))
         os.makedirs(group_data_dir, exist_ok=True)
         tmp_train_file = os.path.join(group_data_dir, "train.json")
         tmp_dev_train_file = os.path.join(group_data_dir, "dev.json")
 
-        count = 0
         with open(tmp_train_file, 'w', encoding="utf-8") as f:
             for line in train_samples:
                 sample = json.loads(line)
-                sample["id"] = count
                 f.write("%s\n"%(json.dumps(sample, ensure_ascii=False)))
-                count += 1
 
         with open(tmp_dev_train_file, "w", encoding="utf-8") as f:
             for line in dev_samples:
                 sample = json.loads(line)
-                sample["id"] = count
                 f.write("%s\n" % (json.dumps(sample, ensure_ascii=False)))
-                count += 1
 
 def k_prompt_for_toefl(data_dir):
     """
@@ -207,13 +330,30 @@ def k_prompt_for_toefl(data_dir):
 
 if __name__ == "__main__":
     ## preprocess gcdc
-    data_dir = "data/dataset"
-    dataset_list = ["gcdc_yahoo", "gcdc_clinton", "gcdc_enron", "gcdc_yelp"]
+    """
+    dataset_list = ["Clinton", "Enron", "Yahoo", "Yelp"]
+    mode_list = ["train", "test"]
     for dataset in dataset_list:
-        print(" Preprocessing %s\n"%(dataset))
-        # k_fold_for_gcdc(data_dir, dataset)
+        print("Processing %s ...."%(dataset))
+        for mode in mode_list:
+            gcdc_csv_reader(dataset, mode)
+        # k_fold for train
+        k_fold_for_gcdc(dataset)
+        # copy test
+        for idx in range(1, 11):
+            command = "cp data/dataset/gcdc_{}/test.json data/dataset/gcdc_{}/{}/".format(
+                dataset.lower(), dataset.lower(), str(idx)
+            )
+            os.system(command)
+        # delete
+        command = "rm -f data/dataset/gcdc_{}/train.json".format(dataset.lower())
+        os.system(command)
+        command = "rm -f data/dataset/gcdc_{}/test.json".format(dataset.lower())
+        os.system(command)
+    """
+    # dataset = "Clinton"
+    # gcdc_csv_reader(dataset, "train")
 
     ## preprocess toefl
-    data_dir = "data/dataset/toefl"
-    # k_prompt_for_toefl(data_dir)
+    toefl_csv_reader()
 
