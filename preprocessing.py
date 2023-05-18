6cc9462a8bc0ab9fc97f96a4c57ca8670d58ca55
liuwei1206 <yanqing_0301>
2023-05-18 15:26:56 +0200

    fix preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,11 @@ import csv
 
 random.seed(106524)
 
+def write_array_into_file(file_name, all_texts):
+    with open(file_name, "w", encoding="utf-8") as f:
+        for text in all_texts:
+            f.write("%s\n"%(text))
+
 def gcdc_csv_reader(dataset, mode):
     """
     convert raw csv file into json
@@ -41,7 +46,7 @@ def gcdc_csv_reader(dataset, mode):
             f.write("%s\n"%(text))
 
 def toefl_csv_reader():
-    label_map = {"0": "1", "1": "2", "2": "3"}
+    label_map = {"0": "low", "1": "medium", "2": "high"}
     file_list = os.listdir("data/dataset/raw/toefl")
     prompt_fold_dict = {}
     for file_name in file_list:
@@ -76,65 +81,21 @@ def toefl_csv_reader():
         for fold_id in range(1, 6):
             fold_dir = os.path.join(data_dir, str(fold_id))
             os.makedirs(fold_dir, exist_ok=True)
+
             train_file = os.path.join(fold_dir, "train.json")
             token = "{}+{}+{}".format(prompt, fold_id, "train")
             print("%s: %d" % (token, len(prompt_fold_dict[token])))
-            with open(train_file, "w", encoding="utf-8") as f:
-                for text in prompt_fold_dict[token]:
-                    f.write("%s\n"%(text))
+            write_array_into_file(train_file, prompt_fold_dict[token])
+
             dev_file = os.path.join(fold_dir, "dev.json")
             token = "{}+{}+{}".format(prompt, fold_id, "valid")
             print("%s: %d" % (token, len(prompt_fold_dict[token])))
-            with open(dev_file, "w", encoding="utf-8") as f:
-                for text in prompt_fold_dict[token]:
-                    f.write("%s\n" % (text))
+            write_array_into_file(dev_file, prompt_fold_dict[token])
+
             test_file = os.path.join(fold_dir, "test.json")
             token = "{}+{}+{}".format(prompt, fold_id, "test")
             print("%s: %d" % (token, len(prompt_fold_dict[token])))
-            with open(test_file, "w", encoding="utf-8") as f:
-                for text in prompt_fold_dict[token]:
-                    f.write("%s\n" % (text))
-        
-
-def toefl_csv_reader_v1(file_name, out_dir):
-
-    csv_reader = csv.reader(open(file_name))
-    fold_id = int(file_name.split("/")[-1].split("_")[-1].split(".")[0])
-    all_texts = []
-    for idx in range(1, 9):
-        all_texts.append([])
-    score_map = {"0": "low", "1": "medium", "2": "high"}
-    cur_line_id = 0
-    for line in csv_reader:
-        cur_line_id += 1
-        if cur_line_id == 1:
-            continue
-
-        essay_id = line[0]
-        prompt = line[1]
-        text = line[4]
-        score = line[3]
-
-        all_texts[int(prompt)-1].append((essay_id, text, score))
-    for idx in range(1, 9):
-        cur_dir_name = os.path.join(out_dir, "toefl_p{}".format(idx))
-        os.makedirs(cur_dir_name, exist_ok=True)
-        cur_fold_dir = os.path.join(cur_dir_name, str(fold_id+1))
-        os.makedirs(cur_fold_dir, exist_ok=True)
-
-        out_name = file_name.split("/")[-1].split("_")[0]
-        out_name = "{}.json".format(out_name)
-        out_name = os.path.join(cur_fold_dir, out_name)
-
-        with open(out_name, "w", encoding="utf-8") as f:
-            for idy, item in enumerate(all_texts[idx-1]):
-                sample = {}
-                sample["id"] = idy
-                sample["essay_id"] = item[0]
-                sample["text"] = item[1]
-                sample["score"] = score_map[item[2]]
-
-                f.write("%s\n"%(json.dumps(sample, ensure_ascii=False)))
+            write_array_into_file(test_file, prompt_fold_dict[token])
 
 def k_fold_for_gcdc(dataset):
     """
@@ -210,123 +171,6 @@ def k_fold_for_gcdc(dataset):
                 sample = json.loads(line)
                 f.write("%s\n" % (json.dumps(sample, ensure_ascii=False)))
 
-def k_prompt_for_toefl(data_dir):
-    """
-    split data for toelf dataset with different prompt, each prompt with 5-fold crossvalidation
-    Args:
-        data_dir:
-        prompt_id:
-    """
-    files = os.listdir(data_dir)
-    files = sorted(files)
-    files = [os.path.join(data_dir, f) for f in files if ".json" in f]
-    prompt_num = len(files)
-
-    # read data from file
-    all_prompts_categories = [[[] for _ in range(3)] for _ in range(prompt_num)]
-    for prompt_id, file_name in enumerate(files):
-        with open(file_name, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            for line in lines:
-                line = line.strip()
-                if line:
-                    sample = json.loads(line)
-                    text = sample['text']
-                    label = sample['score']
-                    prompt = sample['prompt']
-
-                    if label.lower() == 'low':
-                        all_prompts_categories[prompt_id][0].append((prompt, text, label))
-                    elif label.lower() == 'medium':
-                        all_prompts_categories[prompt_id][1].append((prompt, text, label))
-                    elif label.lower() == 'high':
-                        all_prompts_categories[prompt_id][2].append((prompt, text, label))
-
-    # split
-    for prompt_id in range(prompt_num):
-        tmp_all_prompts_categories = None
-        tmp_all_prompts_categories = copy.deepcopy(all_prompts_categories)
-
-        train_dev_high = []
-        train_dev_medium = []
-        train_dev_low = []
-
-        for idx in range(prompt_num):
-            if idx == prompt_id:
-                continue
-            else:
-                train_dev_low.extend(tmp_all_prompts_categories[idx][0])
-                train_dev_medium.extend(tmp_all_prompts_categories[idx][1])
-                train_dev_high.extend(tmp_all_prompts_categories[idx][2])
-
-        random.shuffle(train_dev_low)
-        random.shuffle(train_dev_medium)
-        random.shuffle(train_dev_high)
-        print("low num: ", len(train_dev_low))
-        print("medium num: ", len(train_dev_medium))
-        print("high num: ", len(train_dev_high))
-        # 5-fold cross validation
-        five_group_sample = [[] for _ in range(5)]
-        pivot = 0
-        for item in train_dev_low:
-            five_group_sample[pivot].append(item)
-            pivot += 1
-            pivot = pivot % 5
-
-        for item in train_dev_medium:
-            five_group_sample[pivot].append(item)
-            pivot += 1
-            pivot = pivot % 5
-
-        for item in train_dev_high:
-            five_group_sample[pivot].append(item)
-            pivot += 1
-            pivot = pivot % 5
-
-
-        # write one group into dev.json and other four groups into train.json
-        dir_name = os.path.join("k_fold_toefl", "toefl_p{}".format(prompt_id+1))
-        os.makedirs(dir_name, exist_ok=True)
-
-        for idx in range(5):
-            cur_dir_name = os.path.join(dir_name, "{}".format(idx+1))
-            os.makedirs(cur_dir_name, exist_ok=True)
-            cur_train_file = os.path.join(cur_dir_name, "train.json")
-            cur_dev_file = os.path.join(cur_dir_name, "dev.json")
-            train_samples = []
-            dev_samples = []
-            for idy in range(5):
-                if idx == idy:
-                    dev_samples.extend(five_group_sample[idy])
-                else:
-                    train_samples.extend(five_group_sample[idy])
-
-            # for train
-            count = 0
-            with open(cur_train_file, "w", encoding="utf-8") as f:
-                for sample in train_samples:
-                    now_sample = {}
-                    now_sample['id'] = count
-                    now_sample['prompt'] = sample[0]
-                    now_sample['text'] = sample[1]
-                    now_sample['score'] = sample[2]
-                    count += 1
-
-                    f.write("%s\n"%(json.dumps(now_sample, ensure_ascii=False)))
-
-            # for dev
-            count = 0
-            with open(cur_dev_file, "w", encoding="utf-8") as f:
-                for sample in dev_samples:
-                    now_sample = {}
-                    now_sample['id'] = count
-                    now_sample['prompt'] = sample[0]
-                    now_sample['text'] = sample[1]
-                    now_sample['score'] = sample[2]
-                    count += 1
-
-                    f.write("%s\n" % (json.dumps(now_sample, ensure_ascii=False)))
-
 
 if __name__ == "__main__":
     ## preprocess gcdc
@@ -356,4 +200,3 @@ if __name__ == "__main__":
 
     ## preprocess toefl
     toefl_csv_reader()
-
